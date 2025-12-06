82c7867d23882c1ef18e03a58a41c18c6e4ab56a
lohith.y <lohith.y@BUSOFT-LT182>
2025-12-07 03:17:47 +0530

    New Note

--- notes.js
+++ notes.js
@@ -28,6 +28,11 @@ const notes = [
       title: "Reminder!",
       description: "Hi paps, I hope everything is fine for you there babe. I'm just thinking about you first day unaku yappadi iruku therila but I'm so nervous, Mudinjathum unna paka odii vanthudran thangamey",
       date: "Nov 10, 2025, 05:08 PM"
+    },
+    {
+      title: "I Miss YOUUU",
+      description: "I know you're probably asleep right now, but I just couldn’t sleep myself. I miss you so much... My eyes are longing to see you, and my hands are eager to hold yours. Please papa let me know when you ready lets talk and fix this together..I'm not going to give up on us so easily. Let me take care of it please, I Love you sooo much Ocean ah vida adhigama... I hope you still remember what you said when this song played for the first time. (click the music button)",
+      date: "Dec 07, 2025, 03:08 AM"
     }
   ];
   
